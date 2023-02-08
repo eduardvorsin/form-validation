32321b06495009400b981c7ca679200db931b019
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-02-08 20:53:36 +0600

    added function validationReducer

--- src/js/validation/validationReducer/validationReducer.js
+++ src/js/validation/validationReducer/validationReducer.js
@@ -15,3 +15,26 @@ const ValidationFields = {
   privacyPolicy: 'privacyPolicy',
   communicationMethod: 'communicationMethod',
 };
+
+export default function validationReducer(input) {
+  if (input?.tagName !== 'INPUT') {
+    throw new Error('the passed value must be an input element');
+  }
+
+  switch (input.name) {
+    case ValidationFields.username:
+      return validateUsername(input);
+    case ValidationFields.email:
+      return validateEmail(input);
+    case ValidationFields.password:
+      return validatePassword(input);
+    case ValidationFields.confirmPassword:
+      return validateConfirmPassword(input);
+    case ValidationFields.privacyPolicy:
+      return validatePrivacyPolicy(input);
+    case ValidationFields.communicationMethod:
+      return validateCommunicationMethod(input);
+    default:
+      throw new Error('The validation function is not set for this type of field in the reducer');
+  }
+}
